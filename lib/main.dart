c50d20f52dabc582b845a7e89ff9d662fca05391
Silas Almeida <silas.ms.almeida@gmail.com>
2023-01-01 17:33:30 -0300

    Configure main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:testingbloc_course/api/login_api.dart';
+import 'package:testingbloc_course/api/notes_api.dart';
+import 'package:testingbloc_course/bloc/actions.dart';
+import 'package:testingbloc_course/bloc/app_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:testingbloc_course/dialogs/generic_dialog.dart';
+import 'package:testingbloc_course/dialogs/loading_screen.dart';
+import 'package:testingbloc_course/models/models.dart';
+import 'package:testingbloc_course/strings.dart';
+import 'package:testingbloc_course/views/iterable_list_view.dart';
+import 'package:testingbloc_course/views/login_view.dart';
+
+import 'bloc/app_state.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,9 +40,66 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+    return BlocProvider(
+      create: (context) => AppBloc(
+        loginApi: LoginApi.instance(),
+        notesApi: NotesApi.instance(),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(Strings.homePage),
+        ),
+        body: BlocConsumer<AppBloc, AppState>(
+          listener: (context, state) {
+            //Loading Screen
+            if (state.isLoading) {
+              LoadingScreen.instance().show(
+                context: context,
+                text: Strings.pleaseWait,
+              );
+            } else {
+              LoadingScreen.instance().hide();
+            }
+            //Display Possible errors
+            final loginError = state.loginError;
+            if (loginError != null) {
+              showGenericDialog(
+                context: context,
+                title: Strings.loginErrorDialogTitle,
+                content: Strings.loginErrorDialogContent,
+                optionsBuilder: () => {
+                  Strings.ok: true,
+                },
+              );
+            }
+            //Logged in, but no fetched notes
+            if (state.isLoading == false &&
+                state.loginError == null &&
+                state.loginHandle == const LoginHandle.fooBar() &&
+                state.fetchedNotes == null) {
+              context.read<AppBloc>().add(
+                    const LoadNotesAction(),
+                  );
+            }
+          },
+          builder: (context, state) {
+            final notes = state.fetchedNotes;
+            if (notes == null) {
+              return LoginView(
+                onLoginTapped: ((email, password) {
+                  context.read<AppBloc>().add(
+                        LoginAction(
+                          email: email,
+                          password: password,
+                        ),
+                      );
+                }),
+              );
+            } else {
+              return notes.toListView();
+            }
+          },
+        ),
       ),
     );
   }
